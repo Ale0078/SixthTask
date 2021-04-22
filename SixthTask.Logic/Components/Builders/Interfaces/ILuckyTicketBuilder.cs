255d9cd9e34b242c9329d6a034993e8532378f3a
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-22 10:45:29 +0100

    SixthTask.Logic was changed. Interface ILuckyTicketBuilder was added

---
+++ SixthTask.Logic/Components/Builders/Interfaces/ILuckyTicketBuilder.cs
@@ -0,0 +1,9 @@
+﻿using SixthTask.Logic.Components.Interfaces;
+
+namespace SixthTask.Logic.Components.Builders.Interfaces
+{
+    internal interface ILuckyTicketBuilder
+    {
+        ILuckyTicket Create();
+    }
+}
