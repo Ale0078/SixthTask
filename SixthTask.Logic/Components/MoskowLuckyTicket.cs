893f9daf7b52eb1a9ec8a8e85dff0e55eb73683d
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-22 10:28:53 +0100

    SixthTask.Logic was changed. Class MoskowLuckyTicket was added

---
+++ SixthTask.Logic/Components/MoskowLuckyTicket.cs
@@ -0,0 +1,27 @@
+﻿using SixthTask.Logic.Components.Interfaces;
+
+namespace SixthTask.Logic.Components
+{
+    internal class MoskowLuckyTicket : ILuckyTicket
+    {
+        public bool IsLuckyTicket(params int[] ticket)
+        {
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < ticket.Length; i++)
+            {
+                if ((ticket.Length / 2.0) > i)
+                {
+                    leftSum += ticket[i];
+                }
+                else 
+                {
+                    rightSum += ticket[i];
+                }
+            }
+
+            return leftSum == rightSum;
+        }
+    }
+}
