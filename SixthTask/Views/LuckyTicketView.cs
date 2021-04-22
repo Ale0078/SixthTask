41dc5544c147769c2cfd7f579f93e845ef2a0252
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-22 11:54:06 +0100

    SixthTask was changed. Class LuckyTicketView was added

---
+++ SixthTask/Views/LuckyTicketView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using SixthTask.Logic.UserInterface.Abstracts;
+
+namespace SixthTask.Views
+{
+    public class LuckyTicketView : View
+    {
+        public override void Display(int countOfLuckyTicket)
+        {
+            Console.WriteLine($"Amount of lucky ticket: {countOfLuckyTicket}");
+        }
+    }
+}
