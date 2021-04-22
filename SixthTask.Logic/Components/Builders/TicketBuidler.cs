16a2e777f76a2ef9af12d0ca489b5723f6df6bb5
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-22 10:49:30 +0100

    SixthTask.Logic was changed. Class TicketBuilder was added

---
+++ SixthTask.Logic/Components/Builders/TicketBuidler.cs
@@ -0,0 +1,24 @@
+﻿using SixthTask.Logic.Components.Interfaces;
+using SixthTask.Logic.Components.Builders.Interfaces;
+
+namespace SixthTask.Logic.Components.Builders
+{
+    public class TicketBuidler : ITicketBuilder
+    {
+        public int TicketSize { get; init; }
+        public int TicketMinValue { get; init; }
+        public int TicketMaxValue { get; init; }
+
+        public TicketBuidler(int ticketMinValue, int ticketMaxValue, int ticketSize) 
+        {
+            TicketMinValue = ticketMinValue;
+            TicketMaxValue = ticketMaxValue;
+            TicketSize = ticketSize;
+        }
+
+        public ITicket Create() 
+        {
+            return new Ticket(TicketMinValue, TicketMaxValue, TicketSize);
+        }
+    }
+}
