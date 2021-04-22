1ce26b1a2292077e2b62cee1e1e734655d5ecf34
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-22 10:38:18 +0100

    SixthTask.Logic was changed. Class PiterLuckyTicket was added

---
+++ SixthTask.Logic/Components/PiterLuckyTicket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+using SixthTask.Logic.Components.Interfaces;
+
+namespace SixthTask.Logic.Components
+{
+    internal class PiterLuckyTicket : ILuckyTicket
+    {
+        public bool IsLuckyTicket(params int[] ticket) 
+        {
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < ticket.Length; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    leftSum += ticket[i];
+                }
+                else
+                {
+                    rightSum += ticket[i];
+                }
+            }
+
+            return leftSum == rightSum;
+        }
+    }
+}
