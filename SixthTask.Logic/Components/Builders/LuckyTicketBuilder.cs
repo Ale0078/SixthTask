e07a3d9e99248c974b5f1069b223ac6a41e02524
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-22 10:58:09 +0100

    SixthTask.Logic was changed. Class LuckyTicketBuilder was added

---
+++ SixthTask.Logic/Components/Builders/LuckyTicketBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using SixthTask.Logic.Components.Interfaces;
+using SixthTask.Logic.Components.Builders.Interfaces;
+
+namespace SixthTask.Logic.Components.Builders
+{
+    public class LuckyTicketBuilder : ILuckyTicketBuilder
+    {
+        public string LuckyTicketType { get; init; }
+
+        public LuckyTicketBuilder(string luckyTicketType) 
+        {
+            LuckyTicketType = luckyTicketType;
+        }
+
+        public ILuckyTicket Create() => LuckyTicketType switch
+        {
+            "Moskow" => new MoskowLuckyTicket(),
+            "Piter" => new PiterLuckyTicket(),
+            _ => throw new ArgumentException("Type of lucky tiket must be Moskow or Piter")
+        };
+    }
+}
